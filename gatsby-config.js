9c4570e89586c5d2d539c2b7cb63386ede2c73b1
denghuiquan <980352129@qq.com>
2023-03-16 00:24:26 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `aiecosystem`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
